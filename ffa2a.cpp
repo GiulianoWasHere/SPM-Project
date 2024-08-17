5475dde222283efee46621aff10544790eca0a5d
GiulianoWasHere <giuliano.difranco@hotmail.it>
2024-08-17 19:18:00 +0200

    a2a continue

--- ffa2a.cpp
+++ ffa2a.cpp
@@ -121,6 +121,80 @@ static inline void usage(const char *argv0)
   printf("D - Like d but remove the input file\n");
   printf("--------------------\n");
 }
+/* struct Task_t {
+    Task_t(unsigned char *ptr, size_t size, const std::string &name):
+        ptr(ptr),size(size),filename(name) {}
+
+    unsigned char    *ptr;           // input pointer
+    size_t            size;          // input size
+    unsigned char    *ptrOut=nullptr;// output pointer
+    size_t            cmp_size=0;    // output size
+    size_t            blockid=1;     // block identifier (for "BIG files")
+    size_t            nblocks=1;     // #blocks in which a "BIG file" is split
+    const std::string filename;      // source file name
+}; */
+
+struct Task_t
+{
+  Task_t(const std::string &name) : filename(name) {}
+
+  unsigned char *ptr;              // input pointer
+  size_t size;                     // input size
+  unsigned char *ptrOut = nullptr; // output pointer
+  size_t cmp_size = 0;             // output size
+  size_t blockid = 1;              // block identifier (for "BIG files")
+  size_t nblocks = 1;              // #blocks in which a "BIG file" is split
+  const std::string filename;      // source file name
+};
+struct L_Worker : ff_monode_t<Task_t>
+{ // must be multi-output
+
+  L_Worker(std::vector<FileStruct> &FilesVector, size_t id, size_t numberOfTasks, size_t NumberOfLWorkers)
+      : FilesVector(FilesVector), id(id), numberOfTasks(numberOfTasks), NumberOfLWorkers(NumberOfLWorkers) {}
+
+  Task_t *svc(Task_t *)
+  {
+    // Based on the Id (The id is given at the creation of the node)
+    // the files are divided for each worker
+    for (size_t i = 0; i < numberOfTasks; ++i)
+    {
+      const std::string infilename(FilesVector[id + i*NumberOfLWorkers].filename);
+      size_t infile_size = FilesVector[id + i*NumberOfLWorkers].size;
+      size_t sizeOfT = sizeof(size_t);
+
+      unsigned char *ptr = nullptr;
+      if (!mapFile(infilename.c_str(), infile_size, ptr))
+      {
+        success = false;
+        continue;
+      }
+
+      const size_t fullblocks = infile_size / BIGFILE_LOW_THRESHOLD;
+      const size_t partialblock = infile_size % BIGFILE_LOW_THRESHOLD;
+      
+      // Estimate of the length of the compressed file
+      unsigned long compressedFileLength = mz_compressBound(BIGFILE_LOW_THRESHOLD * fullblocks + partialblock);
+      // ff_send_out(new Task_t(FilesVector[id + i*NumberOfLWorkers].filename));
+    }
+    return EOS;
+  }
+  std::vector<FileStruct> FilesVector;
+  size_t id;
+  size_t numberOfTasks;
+  size_t NumberOfLWorkers;
+  bool success = true;
+};
+struct R_Worker : ff_minode_t<Task_t>
+{ // must be multi-input
+  R_Worker(const size_t Lw) : Lw(Lw) {}
+  Task_t *svc(Task_t *in)
+  {
+    std::cout << in->filename << "\n";
+    // SendToWriter
+    return GO_ON;
+  }
+  const size_t Lw;
+};
 
 int main(int argc, char *argv[])
 {
@@ -156,6 +230,7 @@ int main(int argc, char *argv[])
   bool dir = false;
   std::vector<FileStruct> FilesVector;
 
+  // Walks in the directory and add the filenames in the FileVector
   if (S_ISDIR(statbuf.st_mode))
   {
     success &= walkDirff(argv[2], compressing, FilesVector);
@@ -164,13 +239,42 @@ int main(int argc, char *argv[])
   {
     success &= addFileToVector(argv[2], statbuf.st_size, compressing, FilesVector);
   }
-  
-  vectorSize = FilesVector.size();
-  for (FileStruct i: FilesVector)
+
+  /* vectorSize = FilesVector.size();
+  for (FileStruct i : FilesVector)
     std::cout << i.filename << i.size << "\n";
 
   std::cout << FilesVector[0].filename << "\n";
-  std::cout << vectorSize << "\n";
+  std::cout << vectorSize << "\n"; */
+
+  std::vector<ff_node *> LW;
+  std::vector<ff_node *> RW;
+  size_t numberTasks = FilesVector.size() / Lw;
+  size_t overflowTasks = FilesVector.size() % Lw;
+  for (size_t i = 0; i < Lw; ++i)
+  {
+    if (overflowTasks)
+    {
+      LW.push_back(new L_Worker(FilesVector, i, numberTasks + 1, Lw));
+      overflowTasks--;
+    }
+    else
+      LW.push_back(new L_Worker(FilesVector, i, numberTasks, Lw));
+  }
+  for (size_t i = 0; i < Rw; ++i)
+    RW.push_back(new R_Worker(Lw));
+
+  // Adding Lworkers and Rworkers to a2a
+  ff_a2a a2a;
+  a2a.add_firstset(LW);
+  a2a.add_secondset(RW);
+
+  if (a2a.run_and_wait_end() < 0)
+  {
+    error("running a2a\n");
+    return -1;
+  }
+
   if (!success)
   {
     printf("Exiting with (some) Error(s)\n");
